ab748b827d81aa093704db8d2224d414c2209d80
PrathamPShetty <prathampshetty99sai@gmail.com>
2024-06-23 19:26:14 +0530

    final commit

--- sitslot/settings.py
+++ sitslot/settings.py
@@ -17,12 +17,17 @@ from django.contrib.messages import constants as messages
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9vhls!b*0xpy5j2&_@=y#8q-l@k9876klk1uj&b5qwz(rqldp2'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -137,8 +142,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'snapship43@gmail.com'
-EMAIL_HOST_PASSWORD = 'urif idtw krgj ouej'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 MESSAGES_TAGS={
