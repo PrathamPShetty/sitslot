9058b078bbc5c4333b2dea788b05dc0c49c5dc78
PrathamPShetty <prathampshetty99sai@gmail.com>
2024-10-05 07:46:44 +0530

    commit

--- sitslot/settings.py
+++ sitslot/settings.py
@@ -29,9 +29,11 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'sitslot.adarshsavalagi.me']
+
 
 
 
