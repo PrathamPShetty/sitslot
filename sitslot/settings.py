ab18458eec0c485f56f17c117eff53f8f3e12aa0
PrathamPShetty <prathampshetty99sai@gmail.com>
2024-11-29 13:43:31 +0530

    sd

--- sitslot/settings.py
+++ sitslot/settings.py
@@ -95,12 +95,12 @@ WSGI_APPLICATION = 'sitslot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # DATABASES = {
@@ -113,19 +113,19 @@ WSGI_APPLICATION = 'sitslot.wsgi.application'
 #     }
 # }
 
-from django.db import connection
-connection.queries
+# from django.db import connection
+# connection.queries
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sitslot',  # Replace with your database name
-        'USER': 'pratham',  # Replace with your database username
-        'PASSWORD': '123',  # Replace with your database password
-        'HOST': '172.232.123.61',  # Replace with the IP address or hostname of the remote PostgreSQL server
-        'PORT': '5432',  
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sitslot',  # Replace with your database name
+#         'USER': 'pratham',  # Replace with your database username
+#         'PASSWORD': '123',  # Replace with your database password
+#         'HOST': '172.232.123.61',  # Replace with the IP address or hostname of the remote PostgreSQL server
+#         'PORT': '5432',  
+#     }
+# }
 
 
 
